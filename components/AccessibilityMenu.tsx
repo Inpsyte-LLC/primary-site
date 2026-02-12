ceae6940dec20cc056248985fedc78f5be3595d3
Eric Goldschmidt <eric@MacBookPro.Home>
2026-02-12 16:49:32 -0500

    accessibility man update

--- components/AccessibilityMenu.tsx
+++ components/AccessibilityMenu.tsx
@@ -100,7 +100,7 @@ export default function AccessibilityMenu() {
           <button
             ref={openerRef}
             type="button"
-            className="h-12 w-12 bg-blue-900 rounded-full flex items-center justify-center text-xl outline-none focus:ring-4 focus:ring-teal-500/30"
+            className="h-8 w-8 md:h-10 md:w-10 bg-blue-900 rounded-full flex items-center justify-center text-xl outline-none focus:ring-4 focus:ring-teal-500/30"
             aria-haspopup="dialog"
             aria-expanded={open}
             aria-controls="a11y-dialog"
